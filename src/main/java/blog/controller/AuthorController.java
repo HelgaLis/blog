b40f29e0008e1e1eb19890ae7c225870202f05b2
helga <helga@helga-OEM>
2018-11-20 17:00:27 +0300

    added author controller and several jsp files

--- src/main/java/blog/controller/AuthorController.java
+++ src/main/java/blog/controller/AuthorController.java
@@ -1,10 +1,25 @@
 package blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import blog.service.BlogService;
 
 @Controller
 @RequestMapping("/authors")
 public class AuthorController {
-
+	@Autowired
+	private BlogService blogService;
+	@RequestMapping
+	public String welcome(Model model) {
+		model.addAttribute("welcome", "welcome");
+		return "welcome";
+	}
+	@RequestMapping("/all")
+	public String getAllAuthors(Model model) {
+		model.addAttribute("authors", blogService.getAllUser());
+		return "allAuthors";
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/allAuthors.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>All authors</title>
+</head>
+<body>
+<div>
+<c:forEach var="au" items="${authors}"></c:forEach>
+<p>${au.id}</p>
+<p>${au.name}</p>
+<p>${au.age}</p>
+<p>${au.gender}</p>
+</div>
+</body>
+</html>
